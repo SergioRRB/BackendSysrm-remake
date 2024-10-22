f8fe0b82a000f968fe7c1720e9371fb1c78f1f27
gianbdev <giancarlo.silva550@gmail.com>
2024-10-21 19:44:18 -0500

    fix | selection tarifarios with DTO

--- tarifario-service/src/controllers/selectionTarifario/selectionTarifario.controller.ts
+++ tarifario-service/src/controllers/selectionTarifario/selectionTarifario.controller.ts
@@ -1,32 +1,3 @@
-/*
-import { Request, Response } from "express";
-import { SelectionTarifarioService } from "../../services/selectionTarifarioService/selectionTarifario.service";
-
-const selectionTarifarioService = new SelectionTarifarioService();
-
-export class SelectionTarifarioController {
-  async getTarifario(req: Request, res: Response) {
-    const { id_cliente, id_area, ubigeo, tarifario } = req.query;
-
-    if (!id_cliente || !id_area || !ubigeo || !tarifario) {
-      return res.status(400).json({ message: "Missing required parameters." });
-    }
-
-    try {
-      const tarifas = await selectionTarifarioService.obtenerTarifarioUbigeo(
-        Number(id_cliente),
-        Number(id_area),
-        Number(ubigeo),
-        String(tarifario),
-      );
-
-      return res.json(tarifas);
-    } catch (error) {
-      console.error("Error fetching tarifario:", error);
-      return res.status(500).json({ message: "Error fetching tarifario." });
-    }
-  }
-}*/
 import { Request, Response } from "express";
 import { SelectionTarifarioService } from "../../services/selectionTarifarioService/selectionTarifario.service";
 import { validate } from "class-validator";
