bbb01cdbab17525556f006501f6a6f927dfd60c0
gianbdev <giancarlo.silva550@gmail.com>
2024-11-18 19:09:30 -0500

    in review | asignacion recojo endpoint get

--- comercial-service/src/services/AsignacionRecojo/obtenerAsignacionRecojo.service.ts
+++ comercial-service/src/services/AsignacionRecojo/obtenerAsignacionRecojo.service.ts
@@ -1,41 +1,100 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-// Interfaz para representar los datos de la consulta
+
 export class ObtenerAsignacionRecojoService {
   async obtenerAsignaciones() {
-    const programaciones = await prisma.programaciones.findMany({
-      include: {
-        //id: true, // Selecciona el id de la programación
-        asignacion_recojos: {
-          select: {
-            id: true,
-            id_orden_servicio_recojo: true, // Asignación del recojo
-            id_conductor_recojo: true, // ID del conductor
-            id_auxiliar_recojo: true, // ID del auxiliar
-            nombre_conductor_recojo: true, // Nombre del conductor
-            nombre_auxiliar_recojo: true, // Nombre del auxiliar
-          },
-        },
-        clientes: {
-          select: {
-            razon_social_cliente: true, // Razon social del cliente
-          },
-        },
-        ubigeo: {
-          select: {
-            UBIGEO: true,
-            DEPARTAMENTO: true, // Departamento del ubigeo
-            PROVINCIA: true, // Provincia del ubigeo
-            DESTINO: true, // Destino del ubigeo
-          },
-        },
-      },
-      orderBy: {
-        id: "desc", // Ordenar por id de forma descendente
-      },
-    });
+    try {
+      // Realizar la consulta utilizando Prisma para obtener las asignaciones
+      const asignaciones = await prisma.programaciones.findMany({
+        select: {
+          id: true,
+          id_orden_servicio: true,
+          id_cliente_programacion: true,
+          area_programacion: true,
+          ubigeo_programacion: true,
+          direccion_programacion: true,
+          referencias_programacion: true,
+          contacto_programacion: true,
+          telefono_programacion: true,
+          correo_programacion: true,
+          descripcion_mercancia_programacion: true,
+          cantidad_bultos_programacion: true,
+          peso_mercancia_programacion: true,
+          peso_volumen_programacion: true,
+          metros_cubicos_programacion: true,
+          fecha_programacion: true,
+          hora_programacion: true,
+          id_creador_programacion: true,
+          fecha_creado: true,
 
-    return programaciones;
+          // Relacionar las tablas necesarias
+          /*
+          asignacion_recojos: {
+            select: {
+              id_orden_servicio_recojo: true,
+              id_conductor_recojo: true,
+              id_auxiliar_recojo: true,
+            },
+          },*/
+          asignacion_recojos: {
+            select: {
+              id_orden_servicio_recojo: true,
+              nombre_auxiliar_recojo: true,
+              nombre_conductor_recojo: true,
+            },
+          },
+
+          clientes: {
+            select: {
+              razon_social_cliente: true,
+            },
+          },
+          ubigeo: {
+            select: {
+              DEPARTAMENTO: true,
+              PROVINCIA: true,
+              DESTINO: true,
+            },
+          },
+          /*
+          usuariosConductor: {
+            select: {
+              colaborador_usuario: true,
+            },
+            where: {
+              id: {
+                equals: prisma.asignacion_recojos.id_conductor_recojo,
+              },
+            },
+          },
+          usuariosAuxiliar: {
+            select: {
+              colaborador_usuario: true,
+            },
+            where: {
+              id: {
+                equals: prisma.asignacion_recojos.id_auxiliar_recojo,
+              },
+            },
+          },*/
+        },
+        orderBy: {
+          id: "desc", // Ordenar por el ID en orden descendente
+        },
+      });
+
+      return {
+        success: true,
+        message: "Asignaciones obtenidas correctamente",
+        datos: asignaciones,
+      };
+    } catch (error: any) {
+      return {
+        success: false,
+        message: "Error al obtener asignaciones",
+        error: error.message,
+      };
+    }
   }
 }
